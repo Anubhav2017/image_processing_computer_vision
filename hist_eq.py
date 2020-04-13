15b2cf8a6fa252729ad6d36369ce3f41875c740f
Anubhav2017 <agarwalanubhav.2011@gmail.com>
2020-04-13 16:50:51 +0530

    hist_eq

--- hist_eq.py
+++ hist_eq.py
@@ -4,10 +4,11 @@ from matplotlib import image as mp_image
 import matplotlib.pyplot as plt 
 
 
-def improve_contrast(img):
+def improve_contrast(img1):
     
-    img=cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
+    img=cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
+    # img=img.ravel()
+    # print(img.shape)
     width=img.shape[0]
     height=img.shape[1]
 
@@ -16,6 +17,10 @@ def improve_contrast(img):
     for i in range(width):
         for j in range(height):
             hist[img[i][j]]+=1
+
+    #n,_,_=plt.hist(img)
+    #hist=list(n)
+    #print(hist)
 
     hist=[el/(height*width) for el in hist]
 
@@ -44,10 +49,14 @@ def improve_contrast(img):
 img=cv2.imread("img1.jpeg")
 img2,hist2,img1,hist1=improve_contrast(img)
 
+# img=img.ravel()
+# plt.hist(img)
+# plt.show()
+
 fig=plt.figure()
 plt.title("original")
 fig.add_subplot(2,1,1)
-plt.plot(hist1)
+plt.hist(img1.ravel())
 fig.add_subplot(2,1,2)
 plt.imshow(img1,cmap='gray')
     
@@ -55,7 +64,7 @@ plt.imshow(img1,cmap='gray')
 fig=plt.figure()
 plt.title("converted")
 fig.add_subplot(2,1,1)
-plt.plot(hist2)
+plt.hist(img2.ravel())
 fig.add_subplot(2,1,2)
 plt.imshow(img2,cmap='gray')
 plt.show()
